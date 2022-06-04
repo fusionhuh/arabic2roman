2c529161692367bfeb36f0878d68bd57121f1d52
Josh <joshk559@gmail.com>
2022-06-04 17:45:21 -0500

    working toward it

--- arabic2roman.c
+++ arabic2roman.c
@@ -1,3 +1,10 @@
-int main(int* argc, char* argv[]) {
-	
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* ones[9] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+const char* tens[10] = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C" };
+
+int main(int argc, char* argv[]) {
+	int arabic = atoi(argv[1]);
+	printf("%i", arabic);
 }
