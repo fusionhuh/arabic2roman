fb3c397ac3e2d2094c3e5c3a37251f2693ce6481
Josh <joshk559@gmail.com>
2022-06-04 18:02:50 -0500

    more progress

--- arabic2roman.c
+++ arabic2roman.c
@@ -6,5 +6,18 @@ const char* tens[10] = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
 
 int main(int argc, char* argv[]) {
 	int arabic = atoi(argv[1]);
-	printf("%i", arabic);
+	int roman; 
+
+	//printf("%i", arabic);
+
+	if (arabic > 100 || arabic == 0) {
+		printf("Sorry, this program does not support this number at this time.");
+		return NULL;
+	} else if (arabic < 10) {
+		printf("%s", ones[arabic-1]);
+		return ones[arabic];
+	} else if (arabic % 10 == 0) {
+		printf("%s", tens[(arabic/10)-1]);
+		return tens[arabic];
+	} //else if {}
 }
